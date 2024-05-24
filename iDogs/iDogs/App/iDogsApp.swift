f9a803d63279050885d9f75dd48db2bfc8a4c963
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2024-05-24 14:49:28 -0300

    Configuração Inicial do Projeto

---
+++ iDogs/iDogs/App/iDogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDogsApp.swift
+//  iDogs
+//
+//  Created by Guilherme Nunes Lobo on 24/05/24.
+//
+
+import SwiftUI
+
+@main
+struct iDogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
